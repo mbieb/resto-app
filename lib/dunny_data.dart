bb80fd3d5a1a7c6e6fa2d25a061067df341ab556
habib <muthaarif@gmail.com>
2022-01-24 16:52:47 +0700

    first commit

---
+++ lib/dunny_data.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+import './models/category.dart';
+
+var DUMMY_CATEGORIES = [
+  Category(
+    id: 'c1',
+    title: 'Italian',
+    color: Colors.purple,
+  )
+];
---
+++ lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Resto App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Resto App'),
+      ),
+      body: Center(
+        child: Text(
+          'Hello World',
+        ),
+      ),
+    );
+  }
+}
